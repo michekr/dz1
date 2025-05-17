8e505cfab187482ab7754b169ff31b7f7ef1bf32
miche <vanya0967156586@gmail.com>
2025-05-17 20:30:58 +0300

    дз1

--- дз1.py
+++ дз1.py
@@ -8,3 +8,10 @@ if age > 18:
     print("Вхід дозволено!")
 else:
     print("Вхід заборонено!")
+
+start = int(input(" початкове число: "))
+end = int(input(" кінцеве число: "))
+
+for i in range(start, end + 1):
+    print(i)
+
