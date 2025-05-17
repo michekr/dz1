0170f8216b1a528e86fae22ff455c85a171d4366
miche <vanya0967156586@gmail.com>
2025-05-17 20:15:06 +0300

    дз1

--- дз1.py
+++ дз1.py
@@ -1,3 +1,10 @@
 name = input("ім'я: ")
 age = input("вік: ")
 print(f"Привіт {name}, тобі {age}!")
+
+
+age = int(input(" вік: "))
+if age > 18:
+    print("Вхід дозволено!")
+else:
+    print("Вхід заборонено!")
